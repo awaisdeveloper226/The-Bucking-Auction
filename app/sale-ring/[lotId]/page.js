f1fde8492030ea3f904998425e5b36aabfc25cee
awaisdeveloper226 <muhammadawaisanas555@gmail.com>
2025-08-15 20:02:18 +0500

    sjskjvh

--- app/sale-ring/[lotId]/page.js
+++ app/sale-ring/[lotId]/page.js
@@ -59,7 +59,7 @@ export default function LotDetailsPage() {
   if (!lotData) {
     return (
       <div className="min-h-screen flex items-center justify-center text-gray-500">
-        Lot not found.
+        Loading...
       </div>
     );
   }
