47ac8da7ce626b0fccac71a3e1147cc609f0621a
awaisdeveloper226 <muhammadawaisanas555@gmail.com>
2025-08-20 18:36:39 +0500

    sjddbv

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -70,7 +70,7 @@ export default function SignupPage() {
                 name={field}
                 value={form[field]}
                 onChange={handleChange}
-                placeholder={`Enter your ${field}`}
+                placeholder={`Enter your ${field.replace("cellPhone","cell phone").replace("emailAddress","email").replace("physicalAddress","address").replace("firstName","first name").replace("lastName","last name")}`}
                 required
                 className="mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
